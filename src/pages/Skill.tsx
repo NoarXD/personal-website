93d29aebad2ae63cb4896df5bc35bb4ceba7e7a9
NoarXD <johnnydpx0@gmail.com>
2023-09-26 15:49:27 +0700

    skill3

--- src/pages/Skill.tsx
+++ src/pages/Skill.tsx
@@ -36,63 +36,63 @@ const Skill = () => {
                     <p className="relative left-24 inline bottom-[14px]">Skill</p>
                 </div>
                 <div className="grid grid-cols-2 gap-5">
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/html.png" alt="" width={36} />
                             <p className="ms-3">HTML</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/css.png" alt="" width={30} />
                             <p className="ms-3">CSS</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/JavaScript-logo.png" alt="" width={26} />
                             <p className="ms-3">JavaScript</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/React-icon.png" alt="" width={30} />
                             <p className="ms-3">React</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/Bootstrap_logo.png" alt="" width={30} />
                             <p className="ms-3">Bootstrap</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/tailwind.png" alt="" width={30} />
                             <p className="ms-3">Tailwind</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/mysql.png" alt="" width={30} />
                             <p className="ms-3">Mysql</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/rest-api-1.svg" alt="" width={60} />
                             <p className="ms-3">RestApi</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
                             <img src="/src/img/Node.js_logo.png" alt="" width={30} />
                             <p className="ms-3">NodeJS</p>
                         </div>
                     </div>
-                    <div className="border-[1px] border-orange-300 rounded-md">
+                    <div className="border-[1px] border-orange-300 rounded-md hover:bg-stone-700 duration-500">
                         <div className="m-2 flex items-center">
-                            <img src="/src/img/typescript.png" alt="" width={40} />
+                            <img src="../src/img/typescript.png" alt="" width={40} />
                             <p className="ms-3">TypeScript</p>
                         </div>
                     </div>
