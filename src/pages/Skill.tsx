145eb405ae98d9b8a0ce1e9251a7e225ea92e413
NoarXD <johnnydpx0@gmail.com>
2023-09-26 15:40:16 +0700

    skill2

--- src/pages/Skill.tsx
+++ src/pages/Skill.tsx
@@ -10,7 +10,7 @@ const Skill = () => {
                     <div className="flex flex-col">
                         <div className="flex justify-center">
                             <img
-                                src="src\img\noarlogo_remove.png"
+                                src="/src/img/noarlogo_remove.png"
                                 alt=""
                                 width={200}
                                 className=""
@@ -31,8 +31,71 @@ const Skill = () => {
                         </div>
                     </div>
                 </div>
-                <div className="">
-                    reifhre
+                <div className="mt-5">
+                    <hr className="w-20 border-[1px]" />
+                    <p className="relative left-24 inline bottom-[14px]">Skill</p>
+                </div>
+                <div className="grid grid-cols-2 gap-5">
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/html.png" alt="" width={36} />
+                            <p className="ms-3">HTML</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/css.png" alt="" width={30} />
+                            <p className="ms-3">CSS</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/JavaScript-logo.png" alt="" width={26} />
+                            <p className="ms-3">JavaScript</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/React-icon.png" alt="" width={30} />
+                            <p className="ms-3">React</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/Bootstrap_logo.png" alt="" width={30} />
+                            <p className="ms-3">Bootstrap</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/tailwind.png" alt="" width={30} />
+                            <p className="ms-3">Tailwind</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/mysql.png" alt="" width={30} />
+                            <p className="ms-3">Mysql</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/rest-api-1.svg" alt="" width={60} />
+                            <p className="ms-3">RestApi</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/Node.js_logo.png" alt="" width={30} />
+                            <p className="ms-3">NodeJS</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-orange-300 rounded-md">
+                        <div className="m-2 flex items-center">
+                            <img src="/src/img/typescript.png" alt="" width={40} />
+                            <p className="ms-3">TypeScript</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
